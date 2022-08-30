cb66a837b852301b722f50cf7c2d81b0ce3101df
Gabriel Moraes <gabrielmoraes.dev@gmail.com>
2022-08-30 15:48:43 -0300

    Added new entities and created a second migration

---
+++ SalesWeb/Models/Seller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SalesWeb.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDay { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDay, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDay = birthDay;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord salesRecord)
+        {
+            Sales.Add(salesRecord);
+        }
+
+        public void RemoveSales(SalesRecord salesRecord)
+        {
+            Sales.Remove(salesRecord);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sales => sales.Date >= initial && sales.Date <= final).Sum(sales => sales.Amount);
+        }
+    }
+}
